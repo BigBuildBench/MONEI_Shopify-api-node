e45e9688a47ac8993175abd7fa0a4b2a4f50619a
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-28 09:33:26 +0800

    init instance

---
+++ types/index.test-d.ts
@@ -0,0 +1,59 @@
+import { expectType } from 'tsd';
+import Shopify from '.';
+
+// Can be constructed with public access token.
+new Shopify({
+  shopName: 'my-shopify-store.myshopify.com',
+  accessToken: '111'
+});
+
+// Can be constructed with public access token and version.
+const client = new Shopify({
+  shopName: 'my-shopify-store.myshopify.com',
+  accessToken: '111',
+  apiVersion: '2020-01'
+});
+
+// Can be constructed with Got hooks.
+new Shopify({
+  shopName: 'my-shopify-store.myshopify.com',
+  accessToken: '111',
+  hooks: {
+    beforeRequest: [
+      (options) => {
+        options.headers['X-Test'] = 'test';
+      }
+    ]
+  }
+});
+
+// Accepts the `agent` option.
+new Shopify({
+  shopName: 'my-shopify-store.myshopify.com',
+  accessToken: '111',
+  agent: {
+    // https: new HttpsProxyAgent({
+    //   keepAlive: true,
+    //   keepAliveMsecs: 1000,
+    //   maxSockets: 256,
+    //   maxFreeSockets: 256,
+    //   scheduling: 'lifo',
+    //   proxy: 'https://localhost:8080'
+    // })
+  }
+});
+
+expectType<number>(client.callLimits.remaining);
+expectType<number>(client.callLimits.current);
+expectType<number>(client.callLimits.max);
+
+const shop = await client.shop.get();
+expectType<number>(shop.id);
+expectType<string>(shop.myshopify_domain);
+
+const product = await client.product.get(10);
+expectType<number>(product.id);
+expectType<string>(product.title);
+
+const result = await client.graphql(`query { shop { id myshopify_domain } }`);
+expectType<any>(result);
